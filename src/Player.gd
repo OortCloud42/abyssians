946b7e4bcc7f04992a62c1d432122eb90e4a0639
akinhet <piti200406@gmail.com>
2021-02-15 01:17:14 +0100

    Reworked game's physics

--- src/Player.gd
+++ src/Player.gd
@@ -1,49 +1,59 @@
 extends KinematicBody2D
 
-const INTENDED_FPS = 60
-const ACCELERATION = 12
-const MAX_SPEED = 64
-const FRICTION = 0.25
-const AIR_RESISTANCE = 0.02
-const GRAVITY = 6
-const JUMP_FORCE = 150
+const ACCELERATION = 1
+const MAX_SPEED = 80
+const FRICTION = 1
+const AIR_FRICTION = 0.5
+const GRAVITY = 230
+const JUMP_SPEED = 120
 
 var motion = Vector2.ZERO
 
 onready var sprite = $Sprite
 onready var animationPlayer = $AnimationPlayer
+onready var stateMachine = $AnimationTree.get("parameters/playback")
+onready var animationTree = $AnimationTree
 
 func _physics_process(delta):
-	print(delta)
-	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	
-	if x_input != 0:
-		animationPlayer.play("Run")
-		motion.x += x_input * ACCELERATION * delta * INTENDED_FPS
-		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
-		sprite.flip_h = x_input < 0
-	else:
-		animationPlayer.play("Stand")
-	
-	motion.y += GRAVITY * delta * INTENDED_FPS
+	var direction = get_direction()
+	move_player(delta, direction)
+	play_animations(direction)
+
+
+func get_direction():
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-Input.get_action_strength("jump") if is_on_floor() and Input.is_action_just_pressed("jump") else 0.0)
+
+
+func move_player(delta, direction):
+	motion.y += GRAVITY * delta
 	
 	if is_on_floor():
-		if x_input == 0:
-			#motion.x = lerp(motion.x, 0, 1 - pow(FRICTION, delta))
-			motion.x = lerp(motion.x, 0, FRICTION * delta * INTENDED_FPS)
-		if Input.is_action_just_pressed("ui_up"):
-			motion.y = -JUMP_FORCE
-	else:
-		if motion.y > 0:
-			animationPlayer.play("Jump_down")
+		if direction.y < 0:
+			motion.y = JUMP_SPEED * direction.y
+		if direction.x != 0:
+			motion.x = lerp(motion.x, MAX_SPEED * direction.x, ACCELERATION)
 		else:
-			animationPlayer.play("Jump_up")
-		
-		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
-			motion.y = -JUMP_FORCE/2
-		
-		if x_input == 0:
-			#motion.x = lerp(motion.x, 0, pow(1 - AIR_RESISTANCE, delta))
-			motion.x = lerp(motion.x, 0, AIR_RESISTANCE * delta * INTENDED_FPS)
+			motion.x = lerp(motion.x, 0, FRICTION)
+	else:
+		if direction.x != 0:
+			motion.x = lerp(motion.x, MAX_SPEED * direction.x, AIR_FRICTION)
+		else:
+			motion.x = lerp(motion.x, 0, AIR_FRICTION)
 	
 	motion = move_and_slide(motion, Vector2.UP)
+
+
+func play_animations(direction):
+	sprite.flip_h = direction.x < 0
+	if is_on_floor():
+		if direction.x != 0:
+			stateMachine.travel("Run")
+		else:
+			stateMachine.travel("Stand")
+	else:
+		if motion.y < 0:
+			stateMachine.travel("Jump_up")
+		else:
+			stateMachine.travel("Jump_down")
